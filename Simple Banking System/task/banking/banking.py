7aebd5200990e896dea7920ed308d304b7124499
229898 Janiszewski Łukasz <229898@edu.p.lodz.pl>
2021-02-14 00:21:55 +0100

    Completed stage 1 of the project! It is possible to create account (card) with cardNumber, PIN, balance. There is a simple database (global list variable) that stores all cards created during program run While creating a card object it's number is checked if repeated in the database and in case it was repeated the new one is generated. It is possible to log in your account (card) and check balance which cannot be changed for now.

--- Simple Banking System/task/banking/banking.py
+++ Simple Banking System/task/banking/banking.py
@@ -1,1 +1,112 @@
-# Write your code here
+import random
+import sys
+
+cardsDatabase = []
+
+
+class Card:
+    number = "400000"
+    pin = ""
+    balance = 0
+
+    def __init__(self):
+        global cardsDatabase
+        for i in range(6, 16):
+            self.number = self.number + str(random.randint(0, 9))
+        repeated = False
+        for card in cardsDatabase:
+            if card.number == self.number:
+                repeated = True
+                break
+        while repeated:
+            for i in range(6, 16):
+                self.number[i] = random.randint(0, 9)
+            repeated = False
+            for card in cardsDatabase:
+                if card.number == self.number:
+                    repeated = True
+                    break
+        for i in range(0, 4):
+            self.pin = self.pin + str(random.randint(0, 9))
+        print("Your card has been created\n")
+        print("Your card number:")
+        print(self.getCardNumber())
+        print("Your card PIN:")
+        print(self.getCardPIN())
+
+    def getCardInfo(self):
+        print("Your card number:")
+        print(self.getCardNumber())
+        print("Your card PIN:")
+        print(self.getCardPIN())
+        print("Your card balance:")
+        print(self.getCardBalance())
+
+    def getCardNumber(self):
+        return self.number
+
+    def getCardPIN(self):
+        return self.pin
+
+    def getCardBalance(self):
+        return self.balance
+
+
+
+def createAnAccount():
+    global cardsDatabase
+    card = Card()
+    cardsDatabase.append(card)
+    startProgram()
+
+
+def logIntoAccount():
+    global cardsDatabase
+    print("Enter your card number:")
+    cardNumber = str(input())
+    print("Enter your PIN:")
+    pin = str(input())
+    selectedCard = None
+    for card in cardsDatabase:
+        if card.number == cardNumber:
+            if card.pin == pin:
+                selectedCard = card
+                print("You have successfully logged in!\n")
+            else:
+                print("Wrong card number or PIN!\n")
+        else:
+            print("Wrong card number or PIN!\n")
+    loggedMenu(selectedCard)
+
+
+def loggedMenu(card):
+    print("1. Balance")
+    print("2. Log out")
+    print("0. Exit")
+    option = int(input())
+    if option == 1:
+        print(card.getCardBalance())
+    elif option == 2:
+        print("You have successfully logged out!\n")
+        startProgram()
+    else:
+        print("Bye!\n")
+        sys.exit()
+
+
+def startProgram():
+    print("1. Create an account")
+    print("2. Log into account")
+    print("0. Exit")
+    option = int(input())
+    if option == 1:
+        createAnAccount()
+    elif option == 2:
+        logIntoAccount()
+    else:
+        print("Bye!\n")
+        sys.exit()
+
+
+# ACTUAL PROGRAM START
+startProgram()
